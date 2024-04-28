21297d8d55373054526f55c82194514e17161d48
uday-chandu <udaychowdary503@gmail.com>
2024-04-28 17:00:19 +0530

    files updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='uday-chandu',
+    author_email='udaychowdary503@gmail.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','pyPDF2'],
+    packages=find_packages()
+)
